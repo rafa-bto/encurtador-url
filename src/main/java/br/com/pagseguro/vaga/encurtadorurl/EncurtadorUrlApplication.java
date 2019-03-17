eca2f9d8829089f2cb6e2b6e725954b2e2816e14
Rafa Bto <rafa_bto@yahoo.com.br>
2019-03-17 18:40:34 -0300

    tomcat configuration

--- src/main/java/br/com/pagseguro/vaga/encurtadorurl/EncurtadorUrlApplication.java
+++ src/main/java/br/com/pagseguro/vaga/encurtadorurl/EncurtadorUrlApplication.java
@@ -2,9 +2,10 @@ package br.com.pagseguro.vaga.encurtadorurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EncurtadorUrlApplication {
+public class EncurtadorUrlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncurtadorUrlApplication.class, args);
